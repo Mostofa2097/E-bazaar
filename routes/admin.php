9b696e06f05a55115dc6ce561d93ae1f44805f5f
Mostofa Kamal <mostofakamal3636@gmail.com>
2023-11-10 03:45:43 +0600

    template

--- routes/admin.php
+++ routes/admin.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 
+Route::get('/admin-login', [LoginController::class, 'adminLogin'])->name('admin.login');
 
-
-Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'admin'])->name('admin.home'
-)->middleware('is_admin');
+Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'admin'])->name('admin.home')->
+middleware('is_admin');
 
