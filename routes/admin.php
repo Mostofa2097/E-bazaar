73384181cea4e23be1175fd0c48131846980adee
Mostofa Kamal <mostofakamal3636@gmail.com>
2023-11-14 23:54:41 +0600

    AdminLogout

--- routes/admin.php
+++ routes/admin.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Auth;
 
 Route::get('/admin-login', [LoginController::class, 'adminLogin'])->name('admin.login');
 
-Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'admin'])->name('admin.home')->
-middleware('is_admin');
+// Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'admin'])->name('admin.home')->
+// middleware('is_admin');
 
+Route::group(['namespace'=>'App\Http\Controllers\Admin' , 'middleware'=> 'is_admin'],function(){
+
+    Route::get('/admin/home', 'AdminController@admin')->name('admin.home');
+    Route::get('/admin/logout', 'AdminController@logout')->name('admin.logout');
+});
