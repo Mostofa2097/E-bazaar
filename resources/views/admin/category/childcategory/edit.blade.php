13fc4cf4f5b3e22db4ee26a3b4a974d0562f7557
Mostofa Kamal <mostofakamal3636@gmail.com>
2023-12-01 22:10:01 +0600

    ChildCategory

---
+++ resources/views/admin/category/childcategory/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin')
+
+@section('admin_content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Add new Category') }}</div>
+
+                <div class="card-body">
+                    <form method="post" action="{{route('category.update',$data->id)}}">
+                        @csrf
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">Update Category</label>
+                         
+                          <input type="text" class="form-control  @error('category_name') is-invalid @enderror" 
+                           name="category_name" value="{{ $data->category_name }}" required 
+                          placeholder="Enter Category Name">
+                          @error('category_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">update</button>
+                      </form>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+  @endsection
---
+++ resources/views/admin/category/childcategory/index.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.admin')
+
+@section('admin_content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h4 class="m-0"> Category</h4>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+
+              <button class="btn btn-primary" data-toggle="modal" data-target="#categoryModal">+ Add Category</button>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    
+
+    <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">All Categories list Hear</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+
+                    <table id="example2" class="table table-bordered table-hover table-sm">
+                        <thead>
+                        <tr>
+                          <th>SL</th>
+                          <th>Category Name</th>
+                          <th>Category Slug</th>
+                          <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                          @foreach($data as $key=> $row)
+                          <tr>
+                          <td>{{ $key+1 }}</td>
+                          <td>{{ $row->category_name }}</td>
+                          <td>{{ $row->category_slug }}</td>
+                          <td>
+                          <a href="{{ route('category.edit', $row->id) }}" class="btn btn-info btn-sm "  ><i class="fas fa-edit"></i></a>
+                          <a href="{{ route('category.delete', $row->id) }}" class="btn btn-danger btn-sm" id="delete "><i class="fas fa-trash"></i></a>
+                          </td> </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+                </div>
+              </div>
+            </div>
+        </div>
+     </div>
+    </section>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="categoryModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add New Category</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form method="Post" action="{{route('category.store')}}">
+        @csrf
+        <div class="modal-body">
+          <div class="mb-3">
+            <label for="category_name" class="form-label">Category Name</label>
+            <input type="text" class="form-control" id="category_name" name="category_name" required>
+            <div id="emailHelp" class="form-text">This is you main category</div>
+          </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="Submit" class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+    </div>
+  </div>
+</div>
+
+ 
+
+
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="
+sha512-894YE6QWD5159HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihzOPPSiign/+/3e7J04EaG7TubfWGUrMQ==" crossorigin="
+anonymous"></script>
+<script type="text/javascript">
+$('body').on('click','.edit', function(){
+Let cat_id=$(this).data('id');
+$.get("category/edit/"+cat_id, function(data) {
+$('#e_category_name').val(data.category_name);
+$('#e_category_id').val(data.id);
+});
+});
+</script>
+
+@endsection
